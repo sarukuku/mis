d2427a3f3560a03a62371afbae0ddd27728e9db9
Henri Ylikotila <henri.ylikotila@reaktor.com>
2019-12-03 09:15:08 +0100

    adding prettierrc

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-const webpack = require('webpack');
-const withSass = require('@zeit/next-sass');
-const withCSS = require('@zeit/next-css');
+const webpack = require('webpack')
+const withSass = require('@zeit/next-sass')
+const withCSS = require('@zeit/next-css')
 
-require('dotenv').config();
+require('dotenv').config()
 
 module.exports = withSass({
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty'
-    };
+    }
     /**
      * Returns environment variables as an object
      */
     const env = Object.keys(process.env).reduce((acc, curr) => {
-      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
-      return acc;
-    }, {});
-    config.node = { fs: 'empty' };
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr])
+      return acc
+    }, {})
+    config.node = { fs: 'empty' }
     config.module.rules.push({
       test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
       use: {
@@ -26,9 +26,9 @@ module.exports = withSass({
           limit: 100000
         }
       }
-    });
+    })
 
-    config.plugins.push(new webpack.DefinePlugin(env));
-    return config;
+    config.plugins.push(new webpack.DefinePlugin(env))
+    return config
   }
-});
+})
