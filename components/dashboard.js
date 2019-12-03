2d90bff5873b0106ead0080a90cb5194017d490e
mbonilla <margot.bonilla@reaktor.com>
2019-12-03 15:53:17 +0100

    filter reporter by checkbox #8

---
+++ components/dashboard.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react'
+import Report from './report'
+import fetch from 'isomorphic-unfetch'
+
+const Dashboard = () => {
+  const [reports, setReports] = useState([])
+  const [newReporter, setNewReporter] = useState('')
+  const [filteredReporters, setFilteredReporters] = useState({})
+
+  const handleFilteredReporter = (reporter, checked) => {
+    filteredReporters[reporter] = checked
+    setFilteredReporters({ ...filteredReporters })
+  }
+
+  const fetchReporters = () => {
+    return fetch(`http://localhost:3000/api/report`).then(res => res.json())
+  }
+
+  useEffect(() => {
+    fetchReporters().then(r => {
+      setReports(r)
+
+      // initialize filters
+      r.map(rf => (filteredReporters[rf.reporter] = true))
+      setFilteredReporters({ ...filteredReporters })
+    })
+  }, [])
+
+  const addReporter = async () => {
+    await fetch('/api/report', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ reporter: newReporter })
+    })
+    setNewReporter('')
+    fetchReporters().then(setReports)
+  }
+
+  return (
+    <>
+      <div>
+        {reports.map(r => (
+          <span key={`filter-${r.reporter}`}>
+            <label>{r.reporter}</label>
+            <input
+              type="checkbox"
+              checked={!!filteredReporters[r.reporter]}
+              onChange={e => handleFilteredReporter(r.reporter, e.target.checked)}
+            />
+          </span>
+        ))}
+      </div>
+
+      <div>
+        <input type="text" value={newReporter} onChange={e => setNewReporter(e.target.value)} />
+        <button onClick={addReporter}>Add new Report</button>
+      </div>
+
+      <div className="locations">
+        {reports
+          .filter(r => !!filteredReporters[r.reporter])
+          .map(r => (
+            <Report key={r.reporter} location={r} />
+          ))}
+      </div>
+    </>
+  )
+}
+
+export default Dashboard
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Head from 'next/head'
-import fetch from 'isomorphic-unfetch'
 import './index.scss'
-import Report from '../components/report'
+
+import Dashboard from '../components/dashboard'
 
 const Home = () => {
-  const [reporters, setReporters] = useState([])
-  const [reporter, setReporter] = useState('')
-
-  const fetchReporters = () => {
-    return fetch(`http://localhost:3000/api/report`).then(res => res.json())
-  }
-
-  useEffect(() => {
-    fetchReporters().then(reporters => setReporters(reporters))
-  }, [])
-
-  const addReporter = async () => {
-    await fetch('/api/report', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ reporter })
-    })
-    fetchReporters().then(reporters => setReporters(reporters))
-  }
-
-  const onChangeHandle = text => {
-    setReporter(text)
-  }
-
   return (
     <div>
       <Head>
@@ -38,14 +12,7 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <input type="text" onChange={e => onChangeHandle(e.target.value)} />
-      <button onClick={addReporter}>Add new location</button>
-
-      <div className="locations">
-        {reporters.map(location => (
-          <Report key={location.reporter} location={location} />
-        ))}
-      </div>
+      <Dashboard />
 
       <style jsx>{``}</style>
     </div>
