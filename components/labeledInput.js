566e6d2315bf481cb18985793a8e61de9f7e5872
Henri Ylikotila <henri.ylikotila@gmail.com>
2019-12-04 11:02:14 +0100

    wrapping the input in a form

--- components/labeledInput.js
+++ components/labeledInput.js
@@ -3,13 +3,17 @@ import { TextField } from 'material-ui'
 import { Button } from '@material-ui/core'
 
 export const LabeledInput = ({ value, valueSetter, submitHandler }) => {
+  const submitWrapper = e => {
+    e.preventDefault()
+    submitHandler()
+  }
   return (
-    <div>
+    <form onSubmit={submitWrapper}>
       <TextField value={value} onChange={e => valueSetter(e.target.value)} hintText="Write here a note :)" />
       <Button variant="contained" color="primary" size="small" onClick={submitHandler}>
         Add Note
       </Button>
-    </div>
+    </form>
   )
 }
 
