d1fd44368910aab08ea1aef07aa4d6cee01959a0
Henri Ylikotila <henri.ylikotila@gmail.com>
2019-12-04 10:29:09 +0100

    adding material-ui-core for the button

--- components/labeledInput.js
+++ components/labeledInput.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { TextField } from 'material-ui'
+import { Button } from '@material-ui/core'
 
 export const LabeledInput = ({ value, valueSetter, submitHandler }) => {
   return (
     <div>
       <TextField value={value} onChange={e => valueSetter(e.target.value)} hintText="Write here a note :)" />
-      <button onClick={submitHandler}>Add Note</button>
+      <Button variant="contained" color="primary" onClick={submitHandler}>
+        Add Note
+      </Button>
     </div>
   )
 }
