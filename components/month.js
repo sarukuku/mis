ad02e6e344c65ce9140eedd5dd66e701330a79da
Henri Ylikotila <henri.ylikotila@gmail.com>
2019-12-04 13:50:15 +0100

    new icon button for deleting months

--- components/month.js
+++ components/month.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Topic from './topic/topic'
 import fetch from 'isomorphic-unfetch'
+import { IconButton } from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 const Month = ({ reportId, month, setMonths }) => {
   // TODO show a dialog "Hey, are you sure?"
@@ -19,7 +21,9 @@ const Month = ({ reportId, month, setMonths }) => {
     <div className="month">
       <h3>
         {month.name}
-        <button onClick={() => deleteMonth(reportId, month.id)}>DELETE</button>
+        <IconButton aria-label="delete" size="small" onClick={() => deleteMonth(reportId, month.id)}>
+          <DeleteIcon fontSize="inherit" />
+        </IconButton>
       </h3>
       {month.topics.map(topic => (
         <Topic key={`${topic.name}-${month.id}`} topic={topic} />
