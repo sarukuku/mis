40d4ac3beccd40b0fef7fd61c095a2784afa5135
Henri Ylikotila <henri.ylikotila@gmail.com>
2019-12-04 17:13:22 +0100

    kinda works but looks like s

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import LabeledInput from '../labeledInput'
 import CheckBoxFilterHorizontal from '../checkBoxFilterHorizontal'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
 import './style.scss'
 
 export const Navbar = ({ reports, setReports, newReporter, setNewReporter, addReporter }) => {
   return (
-    <>
-      <CheckBoxFilterHorizontal elements={reports} setElements={setReports} label={'reporter'} />
-      <LabeledInput value={newReporter} valueSetter={setNewReporter} submitHandler={addReporter} hint="Add new reporter" />
-    </>
+    <div>
+      <AppBar>
+        <Toolbar>
+          <CheckBoxFilterHorizontal elements={reports} setElements={setReports} label={'reporter'} />
+          <LabeledInput value={newReporter} valueSetter={setNewReporter} submitHandler={addReporter} hint="Add new reporter" />
+        </Toolbar>
+      </AppBar>
+    </div>
   )
 }
 
