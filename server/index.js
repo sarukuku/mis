c289d7ba247f6b26f542b6e799d71b935cc0e8fd
Joonas Salovaara <joonas.salovaara@gmail.com>
2019-12-29 13:08:20 +0200

    Server fixes
    - Upgraded a few packages
    - Fixed server to work with Heroku
    - Fixed using .env at server
    - Fixed dev script

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const next = require('next')
+const path = require('path')
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000
-const dev = process.env.NODE_DEV !== 'production'
-const nextApp = next({ dev })
-const handle = nextApp.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production'
 const mongoose = require('mongoose')
 const { reportRouter, monthRouter, topicRouter } = require('./routes/index')
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
 
 mongoose
   .connect(process.env.MONGODB_URI, {
@@ -20,23 +22,64 @@ mongoose
     console.log('error connecting to MongoDB:', error.message)
   })
 
-nextApp.prepare().then(() => {
-  const app = express()
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: true }))
+// Multi-process to utilize all CPU cores.
+if (!dev && cluster.isMaster) {
+  console.log(`Node cluster master ${process.pid} is running`)
 
-  app.use('/api/report', reportRouter)
-  app.use('/api/month', monthRouter)
-  app.use('/api/topic', topicRouter)
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
 
-  app.get('*', (req, res) => {
-    return handle(req, res) // react stuff
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`)
   })
+} else {
+  const nextApp = next({ dir: '.', dev })
+  const handle = nextApp.getRequestHandler()
 
-  app.listen(PORT, err => {
-    if (err) {
-      throw err
+  nextApp.prepare().then(() => {
+    const server = express()
+
+    if (!dev) {
+      // Enforce SSL & HSTS in production
+      server.use(function(req, res, next) {
+        var proto = req.headers['x-forwarded-proto']
+        if (proto === 'https') {
+          res.set({
+            'Strict-Transport-Security': 'max-age=31557600' // one-year
+          })
+          return next()
+        }
+        res.redirect('https://' + req.headers.host + req.url)
+      })
     }
-    console.log(`ready at http://localhost:${PORT}`)
+
+    // Static files
+    // https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
+    server.use(
+      '/static',
+      express.static(path.join(__dirname, 'static'), {
+        maxAge: dev ? '0' : '365d'
+      })
+    )
+
+    server.use(bodyParser.json())
+    server.use(bodyParser.urlencoded({ extended: true }))
+
+    server.use('/api/report', reportRouter)
+    server.use('/api/month', monthRouter)
+    server.use('/api/topic', topicRouter)
+
+    server.get('*', (req, res) => {
+      return handle(req, res) // react stuff
+    })
+
+    server.listen(PORT, err => {
+      if (err) {
+        throw err
+      }
+      console.log(`ready at http://localhost:${PORT}`)
+    })
   })
-})
+}
